002903733a8806137ccc0f851b31f69589c3e74e
Tanishq Kancharla <tklightningfast@gmail.com>
2020-07-04 12:26:40 -0400

    Initial Commit

---
+++ PokeDex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeDex
+//
+//  Created by Tanishq Kancharla on 7/4/20.
+//  Copyright © 2020 Tanishq Kancharla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
